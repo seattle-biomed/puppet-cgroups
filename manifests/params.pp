7cf9a33ace1699eab697ea010d7c665ea383e46c
Atom Powers <atom.powers@seattlebiomed.org>
2014-01-30 14:11:20 -0800

    rework config to rely on upstart script

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,15 +10,6 @@ class cgroups::params {
   $config_file  = '/etc/cgconfig.conf'
   $rules_file   = '/etc/cgrules.conf'
 
-  $config_preamble = 'mount {
-    cpu = /sys/fs//cgroup/cpu;
-    cpuacct = /sys/fs/cgroup/cpuacct;
-    devices = /sys/fs/cgroup/devices;
-    memory = /sys/fs/cgroup/memory;
-    freezer = /sys/fs/cgroup/freezer;
-  }
-  '
-
   $service_name = $::osfamily ? {
     'Debian' => 'cgconfig',
     default  => fail('Unsupported Platform')
--- manifests/service.pp
+++ manifests/service.pp
@@ -8,35 +8,14 @@
 class cgroups::service (
   $enable   = $cgroups::service_enable,
   $ensure   = $cgroups::service_ensure,
-  $location = $cgroups::mount_location
 ) {
   include cgroups::params
 
   $service = $cgroups::params::service_name
 
-  $mount_ensure = $ensure ? {
-    'running' => 'present',
-    true      => 'present',
-    default   => 'absent'
-  }
-
-  # Puppet doesn't recognize the special device 'cgroup' so we need to mount all.
-  mount { $location:
-    ensure  => $mount_ensure,
-    device  => 'cgroup',
-    fstype  => 'cgroup',
-    options => 'defaults',
-    dump    => '0',
-    pass    => '0',
-  } ~>
-  exec { 'remount_all_for_cgroups':
-    command => '/bin/mount -a',
-    creates => "${location}/tasks"
-  }
-
   service { $service:
     hasstatus   => true,
-    hasrestart  => true,
+    hasrestart  => false,
   }
 
 }
